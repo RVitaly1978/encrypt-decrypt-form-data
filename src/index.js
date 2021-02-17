2b3cc571efdd65c4cefc8900e57070b049e75d1d
Vitaly Raicheu <RVitaly1978@gmail.com>
2021-02-17 20:45:38 +0300

    feat: add getData function to get input values

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
 import './styles/style.css';
 
-const encryptResult = document.getElementById('encryptResult');
-const decryptResult = document.getElementById('decryptResult');
-console.log(encryptResult, decryptResult);
+const getFormDataNode = () => document.getElementById('formData');
+const getEncryptResultNode = () => document.getElementById('encryptResult');
+
+const getFormData = () => {
+  const formData = getFormDataNode();
+  const inputs = formData.querySelectorAll('input[type=text]');
+  return Array.from(inputs)
+    .filter((item) => item.value)
+    .map(({ id, value }) => ({ id, value }));
+};
+
+const makeRow = (...values) => {
+  const elNode = document.createElement('p');
+  elNode.className = 'row_result';
+
+  values.forEach((value) => {
+    const node = document.createElement('span');
+    node.className = 'item_result';
+    node.innerText = value;
+    elNode.appendChild(node);
+  });
+
+  return elNode;
+};
+
+const makeContent = (data) => {
+  const content = data.map((item) => makeRow(...Object.values(item)));
+  return content;
+};
+
+const handleSubmit = (evt) => {
+  evt.preventDefault();
+
+  const data = getFormData();
+  const content = makeContent(data);
+
+  const container = getEncryptResultNode();
+  container.innerHTML = '';
+
+  content.forEach((item) => container.appendChild(item));
+  container.classList.add('containerVisible');
+};
+
+window.addEventListener('load', () => {
+  const formData = getFormDataNode();
+  formData.addEventListener('submit', handleSubmit);
+});
